3ae9856cd32f6473254f1175e92669ae581a339c
Swesebbe91 <bassepersson@hotmail.com>
2021-11-25 16:16:52 +0100

    Updated with catching card clicks

--- src/App.jsx
+++ src/App.jsx
@@ -2,57 +2,56 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import Card from "./Components/Card";
 
-
-//Skapa ett memory
-//Skapa ett objekt att hålla korten i
-//Skapa en lista, duplicera listan
-//Skapa id
-//Skapa image
-//value
-
-//ID === ID, TA BORT FRÅN LISTAN
-//OM ARRAY === {0} = GAME OVER
-//RESET FETCH CARD
-
 function App() {
 const[data, setData] = useState([]);
-const [image, setImg] = useState(false);
-
-// let fiveCards = [];
-// let duplicate = [];
-// let combined = [];
+let [testArray, setTestArray] = useState ([]);
 
   useEffect(() => {
-    fetchData("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+    fetchData("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"); //Get deck
   },[] );
 
   async function fetchData(url) {
     const res = await fetch(url);
     const data = await res.json();
    
-    const newDeck = await fetch(`https://deckofcardsapi.com/api/deck/${data.deck_id}/draw/?count=52`);
+    const newDeck = await fetch(`https://deckofcardsapi.com/api/deck/${data.deck_id}/draw/?count=52`); // Cards
     const newData = await newDeck.json();
     const cards = newData.cards
-    createObjectsFunction(cards)
+    addObjects(cards) //add ID & match object to the array
   }
 
-  
-    const createObjectsFunction = (cards) => { //Funktion med argument cards
-    const shuffledCards = [...cards] //Kopia av cards
+  useEffect(() => {
+
+  }, [testArray]);
+
+    const addObjects = (cards) => { 
+    cards = [...cards] //Kopia av cards
     .map((cards, i) => ({...cards, id: i, match: false})) //Gå igenom listan, sätt id, matchning falskt
-  
-    setData(shuffledCards) //Sätt ut datan
-    console.log(shuffledCards)
+    setData(cards) //Sätt ut datan
+
   }
 
+  const handleClick = (id) => {
+    console.log("Click")
+    setTestArray(() => [...testArray, id] )
+    console.log(id);
+    
+  }
+  
+  console.log(testArray);
   return (
     <div className="App">
       <h2>Welcome to my memory game! </h2>
       <div className="flex">
-      {data.map((item) => (
-        <Card key={item.id} items = {item}/>
-      
+      {data.map((item, id) => (
+        <Card key={id}
+         image = {item.image}
+         checked = {testArray.includes(id)}
+         value = {item.value}
+         onClicked ={() => handleClick(id)}
+          />
       ))}
+     
 
     </div>
     </div>
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,21 +1,25 @@
 import React from "react";
-import { useState } from "react/cjs/react.development";
+import { useState } from "react";
 import "./Card.css";
 import pokemonCard from "./pokemonCard.jpg";
 
 const Card = (props) => {
-    const [newPicture, setNewPicture] = useState(false)
+//const [newPicture, setNewPicture] = useState(false)
     
   // const changePicture = () => {
   // <img className="img-size" src={props.items.image}></img>
 
   return (
-    <div className="d-flex">
-        
-      <img src={pokemonCard}  className="img-size" alt="Background of a pokemon card"></img>
-      <img className="img-size" src={props.image ? props.items.image : pokemonCard}  onClick={(props.onClick)} />
+    <div className="d-flex"> 
+      {/* <img className="img-size" src={newPicture ?
+      //  image : pokemonCard}  onClick={() => setNewPicture((prev) => !prev)}/> */}
+
+          <img className="img-size" src={props.checked ? props.image : pokemonCard }  onClick ={props.onClicked}/>
     </div>
-    
+    //Få att max två kort att vänder sig
+    //Om sant => låt korten ligga kvar uppvända
+    //Om falskt => Vänd tillbaka korten till "False"
+    //
   );
 };
 
